d406ec5e43a8d9e6b8f347b7e8feb1291a3bf4a9
IvoryGould <ivory.gould@gmail.com>
2019-10-22 16:58:49 +1100

    Complex Memes Updated

---
+++ Complex Memes/Assets/CameraMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    Transform t;
+    GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+
+        t = this.transform;
+        camera = GameObject.Find("Main Camera");
+
+	}
+
+    [SerializeField]
+    public float moveSpeed;
+    [SerializeField]
+    public float rotateSpeed;
+    [SerializeField]
+    public float zoomSpeed;
+
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal") * moveSpeed;
+        float v = Input.GetAxis("Vertical") * moveSpeed;
+        float y = Input.GetAxis("YAxis") * moveSpeed;
+
+        this.transform.Translate(h * Time.deltaTime, y * Time.deltaTime, v * Time.deltaTime);
+
+        t.eulerAngles = new Vector3(t.eulerAngles.x, t.eulerAngles.y, 0);
+
+        if (Input.GetMouseButton(1)) {
+
+            float mh = Input.GetAxis("Mouse X") * rotateSpeed;
+            float mv = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+            this.transform.Rotate(mv * Time.deltaTime, mh * Time.deltaTime, 0);
+
+        }
+
+        float ms = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        camera.transform.Translate(0, 0, ms * Time.deltaTime);
+
+	}
+
+}
---
+++ Complex Memes/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject characterSheet;
+
+	// Use this for initialization
+	void Start () {
+
+        characterSheet = GameObject.Find("CharacterSheet");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+
+            characterSheet.SetActive(!characterSheet.activeSelf);
+
+        }
+
+	}
+}
